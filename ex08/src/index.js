e0fb93d7ba5b79beac003dde0d16f35425a96160
farepule <faruk.puska@gmail.com>
2021-03-20 10:50:14 +0100

    ex 08

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,4 +1,4 @@
-va contacts = [
+var contacts = [
     {
         "firstName": "John",
         "lastName": "Doe",
@@ -25,8 +25,22 @@ va contacts = [
     }
 ];
 function lookUpProfile(name, prop){
-    
+    for (var i=0;i<contacts.length;i++){
+        if (contacts[i].firstName==name){
+          if (prop in contacts[i]){
+            return contacts[i][prop];
+          } else {
+            return "No such property"
+          }
+        }
+      }
+      return "No such contact"
 
 }
-console.log(lookUpProfile("Johan", "likes"));
+console.log(lookUpProfile("John", "likes"));
+console.log(lookUpProfile("Jane", "lasName"));
+console.log(lookUpProfile("Richard", "likes"));
+console.log(lookUpProfile("Rob", "number"));
+console.log(lookUpProfile("Rob", "JavaScript"));
+console.log(lookUpProfile("John", "address"));
 module.exports = lookUpProfile;
